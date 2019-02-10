7eedfa4bb3886494d87422edb465069f8e2073fd
Aziz Ali <aziz@ilovecoding.org>
2019-02-10 16:47:54 -0600

    Convert Counter to Class Component

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -1,17 +1,34 @@
 import React from 'react'
 
-function Counter(props){
-  // Wrong way to track values that need to be rendered
-  let num = props.initialValue
+// function Counter(props){
+//   // Wrong way to track values that need to be rendered
+//   let num = props.initialValue
 
-  return(
-    <div onClick={()=>{ 
-      num++
-      alert('Counter is: ' + num)
-    }}>
-      I am a counter {num}
-    </div>
-  )
+//   return(
+//     <div onClick={()=>{ 
+//       num++
+//       alert('Counter is: ' + num)
+//     }}>
+//       I am a counter {num}
+//     </div>
+//   )
+// }
+
+class Counter extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    let num = this.props.initialValue
+    return(
+      <div onClick={()=>{ 
+        num++
+        alert('Counter is: ' + num)
+      }}>
+        I am a counter {num}
+      </div>
+    )
+  }
 }
 
 export default Counter
